31df20fb01f02ded49cb381732ed0aa8d3813a00
GIWTO <3033291479@qq.com>
2022-05-01 16:20:28 +0800

    change

--- archwsl-config/install.sh
+++ archwsl-config/install.sh
@@ -70,6 +70,7 @@ printUpdate "Setting up zsh"
 sudo -i -u $username bash << EOF
 cd ~
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | /bin/bash
+curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh | /bin/bash
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 wget https://raw.githubusercontent.com/GIWTO/dotfiles/master/home-dotfiles/p10k
 wget https://raw.githubusercontent.com/GIWTO/dotfiles/master/home-dotfiles/zshrc
