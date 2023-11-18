d63ea0dc62242bfd309106b1b60800e14cb050b9
GIWTO <3033291479@qq.com>
2023-11-18 16:13:36 +0800

    Update

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 设置 Git 仓库的本地路径
+localRepoPath="/c/Users/wang/.dotfiles"
+
+# 切换到 Git 仓库目录
+cd "$localRepoPath" || exit
+
+# 设置 Git 安装路径
+gitPath="/c/Users/wang/scoop/apps/git/current/cmd"
+
+# 设置日志文件路径
+scoopExportFile="$localRepoPath/scoop_export.json"
+
+# 备份 Scoop 软件列表
+scoop export > "$scoopExportFile"
+
+# 切换回 Git 仓库目录
+cd "$localRepoPath" || exit
+
+# 执行 Git 操作
+"$gitPath/git" add .
+"$gitPath/git" commit -m "Update"
+
+# 先执行 git push
+"$gitPath/git" push
+
+echo "执行完毕"
